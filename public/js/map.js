9fc1a7287dc29d78f184aeecae4b61e071033c2b
Vedansh Gupta <waydansh@gmail.com>
2024-07-19 00:06:17 +0530

    map marker

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,14 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    center: [77.359473, 28.613097], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 12 // starting zoom
 });
+
+console.log(coordinates)
+
+const marker1 = new mapboxgl.Marker()
+        .setLngLat(coordinates)
+        .addTo(map);
+
+console.log(map)
