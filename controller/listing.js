b613b03b3a57770cea5c383ddb6a71d85b024bfd
Vedansh Gupta <waydansh@gmail.com>
2024-07-18 23:26:55 +0530

    type point

--- controller/listing.js
+++ controller/listing.js
@@ -18,14 +18,14 @@ module.exports.addNewListing = async (req, res, next) => {
         limit: 1
       })
         .send()
-    console.log(response.body.features[0].geometry)
-    res.send("done!")
     let url = req.file.path
     let filename = req.file.filename
     const newListing = new Listing(req.body.listing)
     newListing.owner = req.user._id
     newListing.image = { url, filename }
-    await newListing.save()
+    newListing.geometry = response.body.features[0].geometry
+    let savedListing =  await newListing.save()
+    console.log(savedListing)
     req.flash("success", "New Listing added!")
     res.redirect("/listings")
 }
