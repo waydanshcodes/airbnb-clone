a1046d63a3380d5a9d8b1ae30b05168d78f2879f
Vedansh Gupta <waydansh@gmail.com>
2024-07-21 22:44:50 +0530

    done with maps

--- public/js/map.js
+++ public/js/map.js
@@ -1,15 +1,19 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    style: "mapbox://styles/mapbox/streets-v12",  // style URL
+    center: listing.geometry.coordinates, // [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 12 // starting zoom
 });
 
-console.log(coordinates)
+console.log(listing.geometry.coordinates)
 
-const marker1 = new mapboxgl.Marker()
-    .setLngLat(coordinates)
-    .setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML("<h1>Hello World!</h1>"))
+const marker1 = new mapboxgl.Marker({ color: "red" })
+    .setLngLat(listing.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(
+        `<h4>${listing.title}</h4>
+        <p><i>Exact location will be provided after booking</i></p>`
+    ))
     .addTo(map);
 
 console.log(map)
